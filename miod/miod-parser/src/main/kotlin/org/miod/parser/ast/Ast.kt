d0147d95ebf00bb391e6041176d6bdcc6b7f1d8f
Yury Benesh <ybxsoft@tut.by>
2021-09-12 22:29:59 +0300

    made kotlin classes contain properties

--- miod/miod-parser/src/main/kotlin/org/miod/parser/ast/Ast.kt
+++ miod/miod-parser/src/main/kotlin/org/miod/parser/ast/Ast.kt
@@ -1,18 +1,23 @@
 package org.miod.parser.ast
 
-class Location(path: java.nio.file.Path, line: Int, column: Int)
-class Comment(location: Location, text: String)
-class DocComment(location: Location, text: String)
-class Import(location: Location, name: String, all: Boolean)
-class Unit(location: Location, name: String, comment: Comment, doc: DocComment, imports: Array<Import>)
+import java.nio.file.Path
 
-abstract class LiteralValue(location: Location)
-class LiteralIntegerValue (location: Location, value: Long) : LiteralValue(location)
-class LiteralBooleanValue (location: Location, value: Boolean) : LiteralValue(location)
-class LiteralFloatValue (location: Location, value: Double) : LiteralValue(location)
-class LiteralStringValue (location: Location, value: String) : LiteralValue(location)
-class LiteralStringFromIdValue(location: Location, value: String) : LiteralValue(location)
+class Location(val path: Path, val line: Int, val column: Int)
+class Comment(val location: Location, val text: String)
+class DocComment(val location: Location, val text: String)
+class Import(val location: Location, val name: String, val all: Boolean,
+             val annotations: Array<Annotation>)
+class Unit(val location: Location, val name: String, val comment: Comment,
+           val doc: DocComment, val imports: Array<Import>)
 
-class Annotation(location: Location, name: String, values: Map<String, LiteralValue>)
+abstract class LiteralValue(val location: Location)
+class LiteralIntegerValue (location: Location, val value: Long) : LiteralValue(location)
+class LiteralBooleanValue (location: Location, val value: Boolean) : LiteralValue(location)
+class LiteralFloatValue (location: Location, val value: Double) : LiteralValue(location)
+class LiteralStringValue (location: Location, val value: String) : LiteralValue(location)
+class LiteralStringFromIdValue(location: Location, val value: String) : LiteralValue(location)
 
-class Const(location: Location, name: String, value: LiteralValue)
+class Annotation(val location: Location, val name: String, val values: Map<String, LiteralValue>)
+
+class Const(val location: Location, val name: String, val value: LiteralValue,
+            val annotations: Array<Annotation>)
